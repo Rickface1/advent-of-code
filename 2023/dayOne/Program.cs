95001d3646e6d1ddc5f5b1069e01be4d9448f14c
Erick Whatcott <grimmreeperjr@gmail.com>
2023-12-02 12:46:17 -0700

    New Repository

---
+++ 2023/dayOne/Program.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Reflection.Metadata.Ecma335;
+
+class Pair{
+    public String str;
+    public int value;
+    public String name;
+
+    public Pair(string str, int value){
+        this.str = str;
+        this.value = value;
+        this.name = StrRepresentation();
+    }
+
+    public String StrRepresentation(){
+        return int.Parse(str) switch
+        {
+            1 => "one",
+            2 => "two",
+            3 => "three",
+            4 => "four",
+            5 => "five",
+            6 => "six",
+            7 => "seven",
+            8 => "eight",
+            9 => "nine",
+            _ => "",
+        };
+    }
+}
+class DayOne{
+    private static List<Pair> pairs = [
+        new Pair("1",1),
+        new Pair("2",2),
+        new Pair("3",3),
+        new Pair("4",4),
+        new Pair("5",5),
+        new Pair("6",6),
+        new Pair("7",7),
+        new Pair("8",8),
+        new Pair("9",9)
+    ];
+    
+    static async Task Main(string[] args){
+        string[] list = File.ReadAllLines("input.txt");
+        Func<int> bob = () => MainProgram(list);
+        double[] time = await IterateWithTime(50000,bob);
+        Console.WriteLine("Average Time:");
+        Console.WriteLine(time.GetValue(1));
+        Console.WriteLine("Values:");
+        Console.WriteLine(time.GetValue(0));
+    }
+
+    public async static Task<double[]> IterateWithTime(int times, Func<int> function){
+        List<float> timeLog = [];
+        List<int> values = [];
+
+        await Task.Run(function);
+
+        Stopwatch sw = new();
+        sw.Start();
+
+        for(int y = 0; y < times; y++){
+            await Task.Run(function);
+        }
+
+        sw.Stop();
+
+        double timeElapsed = sw.Elapsed.TotalMilliseconds / times;
+
+        double[] arr = [function(), timeElapsed];
+        return arr;
+    }
+
+
+    public static int MainProgram(String[] list){
+        int counter = 0;
+
+       Parallel.ForEach(list, var =>{
+            int currentVar = Parse(var);
+            Interlocked.Add(ref counter, currentVar);
+        });
+        return counter;
+    }
+
+    public static int Parse(string var){
+        int startValue = 0;
+        int endValue = 0;
+
+        int startIndex = int.MaxValue;
+        int endIndex = int.MinValue;
+
+        foreach (Pair p in pairs){
+            int firstIndex = var.IndexOf(p.str);
+
+            int otherIndex = var.IndexOf(p.name);
+
+            bool firstBool = firstIndex == -1 ? new Func<bool>(() => { firstIndex = int.MaxValue; return false; })() : true;
+            bool otherBool = otherIndex == -1 ? new Func<bool>(() => { otherIndex = int.MaxValue; return false; })() : true;
+
+            if (firstBool && otherBool){
+                int minValue = Math.Min(firstIndex, otherIndex);
+                if (minValue < startIndex){
+                    startValue = p.value;
+                    startIndex = minValue;
+                }
+
+                int maxValue;
+                bool met = false;
+
+                int lastIndex = var.LastIndexOf(p.str);
+                lastIndex = lastIndex == -1 ? new Func<int>(() => {
+                    met = true;
+                    return int.MinValue;
+                })() : lastIndex;
+                
+                int otherLastIndex = var.LastIndexOf(p.name);
+                if(otherLastIndex == -1){
+                    if(met == true){
+                        continue;
+                    }else{
+                        otherLastIndex = int.MinValue;
+                    }
+                }
+
+                maxValue = Math.Max(lastIndex, otherLastIndex);
+                if (maxValue > endIndex){
+                    endValue = p.value;
+                    endIndex = maxValue;
+                }
+            }else if(firstBool){
+                if (firstIndex < startIndex){
+                    startValue = p.value;
+                    startIndex = firstIndex;
+                }
+
+                int lastIndex = var.LastIndexOf(p.str);
+
+                if (lastIndex > endIndex){
+                    endValue = p.value;
+                    endIndex = lastIndex;
+                }
+            }else if (otherBool){
+                if (otherIndex < startIndex){
+                    startValue = p.value;
+                    startIndex = otherIndex;
+                }
+                
+                int otherLastIndex = var.LastIndexOf(p.name);
+
+                if (otherLastIndex > endIndex){
+                    endValue = p.value;
+                    endIndex = otherLastIndex;
+                }
+            }
+        }
+
+        return int.Parse(startValue.ToString() + endValue.ToString());
+    }
+}
---
+++ 2023/dayTwo/Program.cs
@@ -0,0 +1,174 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+class ColorCombinations{
+    public string color;
+    public int quantity;
+
+    public ColorCombinations(string color, int quantity){
+        this.color = color;
+        this.quantity = quantity;
+    }
+}
+class AdventDayTwo{
+    static void Main(string[] args){
+        string[] list = File.ReadAllLines("input.txt");
+
+        Func<double> bob = () => MainProgram(list);
+        double[] time = IterateWithTime(10000, 100, bob);
+
+        Console.WriteLine("Old Way:\n");
+        Console.WriteLine("Average Time:");
+        Console.WriteLine(time.GetValue(1));
+        Console.WriteLine("Values:");
+        Console.WriteLine(time.GetValue(0));
+
+        bob = () => NewMainProgram(list);
+        time = IterateWithTime(10000, 100, bob);
+
+        Console.WriteLine("\nNew Way:\n");
+        Console.WriteLine("Average Time:");
+        Console.WriteLine(time.GetValue(1));
+        Console.WriteLine("Values:");
+        Console.WriteLine(time.GetValue(0));
+    }
+    public static double[] IterateWithTime(int times, int warmups, Func<double> function){
+        for(int x = 0; x < warmups; x++){
+            function();
+        }
+
+        Stopwatch sw = new();
+        sw.Start();
+
+        for(int y = 0; y < times; y++){
+            function();
+        }
+
+        sw.Stop();
+
+        double timeElapsed = sw.Elapsed.TotalMilliseconds / times;
+
+        double[] arr = [function(), timeElapsed];
+        return arr;
+    }
+    static int MainProgram(string[] list){
+        int value = 0;
+
+        Parallel.ForEach(list,input =>{
+            int currentVar = Parse(input[(input.IndexOf(':') + 2)..]);
+            Interlocked.Add(ref value, currentVar);
+        });
+
+        return value;
+    }
+
+    public static int NewMainProgram(string[] list){
+        int value = 0;
+
+        Parallel.ForEach(list,input =>{
+            int currentVar = NewParse(input[(input.IndexOf(':') + 2)..].ToCharArray());
+            Interlocked.Add(ref value, currentVar);
+        });
+
+        return value;
+    }
+
+    public static int NewParse(char[] input){
+        int globalGreen = 0, globalRed = 0, globalBlue = 0;
+        int green = 0, red = 0, blue = 0;
+
+        for(int y = 0; y < input.Length; y++){
+            if(char.IsDigit(input[y])){
+                int index = input.AsSpan(y).IndexOf(' ');
+
+                index = index == -1 ? input.Length - 1 : index + y;
+
+                int currentValue = int.Parse(new string(input.AsSpan(y, index - y)));
+
+                char constant = input[index + 1];
+
+                switch(constant){
+                    case 'b':
+                        blue += currentValue;
+                        break;
+                    case 'g':
+                        green += currentValue;
+                        break;
+                    case 'r':
+                        red += currentValue;
+                        break;
+                }
+                int semicolonIndex = input.AsSpan(y).IndexOf(';');
+                semicolonIndex = semicolonIndex < 0 ? int.MaxValue : semicolonIndex + y;
+
+                int commaIndex = input.AsSpan(y).IndexOf(',');
+                commaIndex = commaIndex < 0 ? int.MaxValue : commaIndex + y;
+
+                if (!(semicolonIndex == int.MaxValue && commaIndex == int.MaxValue))
+                    y = Math.Min(semicolonIndex, commaIndex) - 1;
+                else break;
+            }else if(input[y] == ';'){
+                globalBlue = Math.Max(blue, globalBlue);
+                blue = 0;
+
+                globalGreen = Math.Max(green, globalGreen);
+                green = 0;
+
+                globalRed = Math.Max(red, globalRed);
+                red = 0;
+            }
+        }
+
+        int returnValue = Math.Max(blue, globalBlue) * Math.Max(green, globalGreen) * Math.Max(red, globalRed);
+        return returnValue;
+    }
+
+    public static int Parse(string input){
+        int globalGreen = 0;
+        int globalRed = 0;
+        int globalBlue = 0;
+
+        string[] splitStr = input.Split(';');
+        foreach(string strGlobal in splitStr){
+            string[] strArray = strGlobal.Split(", ");
+            int green = 0;
+            int blue = 0;
+            int red = 0;
+
+            foreach(string str in strArray){
+                int counter = 0;
+                int localPlaceHolder = 0;
+                string value = "";
+
+                while(char.IsWhiteSpace(str[localPlaceHolder + counter])){
+                    counter++;
+                }
+
+                while(char.IsNumber(str[localPlaceHolder + counter])){
+                    value += str[localPlaceHolder + counter];
+                    counter++;
+                }
+
+                string updatedStr = str.Substring(1 + counter);
+                int finalValue = int.Parse(value);
+
+                switch(updatedStr){
+                    case "green":
+                        green += finalValue;
+                        globalGreen = Math.Max(globalGreen, green);
+                        break;
+                    case "red":
+                        red += finalValue;
+                        globalRed = Math.Max(globalRed, red);
+                        break;
+                    case "blue":
+                        blue += finalValue;
+                        globalBlue = Math.Max(globalBlue, blue);
+                        break;
+                }
+            }
+        }
+
+        return globalGreen * globalRed * globalBlue;
+    }
+}
