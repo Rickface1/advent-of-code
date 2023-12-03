6659baef5576f68cb5844ef353a236aea7b28b5d
Erick Whatcott <grimmreeperjr@gmail.com>
2023-12-03 13:42:22 -0700

    Add demonstation class

---
+++ ExampleDay.cs
@@ -0,0 +1,7 @@
+namespace y2023;
+
+public class DayNumber(string filePath) : main.CalendarCode(filePath){
+    public override void Execute(){
+        Console.WriteLine("Example Thing");
+    }
+}
